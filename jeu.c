71ece64f3439ab36a32032e921d47b6d1c41eb6b
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-16 12:50:30 +0100

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -18,6 +18,12 @@
 #define TAILLE_BONUS_EXPLOSION 6     //Bonus imposé par la consigne
 
 char grille[HAUTEUR][LARGEUR];		//Déclaration de la variable globale grille (matrice de taille HAUTEUR*LARGEUR) utilisé dans la majorité des sous-programme de ce module
+typedef struct {
+    int trouve;     // 0 = aucune suite, 1 = suite trouvée
+    int ligne;
+    int colonne;
+    int taille;
+} ResultatSuite; //structure à utiliser pour eliminer la suite
 
 void viderGrille() {
 	for (int i=0; i<HAUTEUR; i++) {
@@ -33,13 +39,62 @@ int verifierAbsencesFiduresInitiales () {
 	}
 }
 
-eliminerSuiteEnLigne(int ligne, int colonne, int taille) {
+ResultatSuite detecterSuiteEnLigne(char grille[HAUTEUR][LARGEUR]){
+    int i, j;
+    ResultatSuite resultatL = {0, -1, -1, 0}; //initialisation du retour de la fonction avec des valeurs invalides (-1) pour ne pas créer de bug
+    //boucle for pour parcourir les lignes donc la hauteur
+    for(i=0; i<HAUTEUR; i++){
+        int compteur = 1; //sert à mesurer la longueur d’une suite d’items identiques consécutifs. s'incrémente quand deux cases consécutives sont égales
+        //boucle for pour parcourir les colonnes donc la largeur
+        for(j=1; j<LARGEUR; j++){
+            if(grille[i][j]==grille[i][j-1] && grille[i][j] != ' '){ //comparaison de deux cases consécutives
+                compteur++;
+    
+                if(compteur>=3){ //on a une suite valide si elle fait au moins 3 cases
+                    resultatL.trouve = 1;
+                    resultatL.ligne = i; //on veut le numero de la ligne où se trouve la suite
+                    resultatL.colonne = j - compteur + 1;//on veut le numero de la première colonne où commence la suite
+                    resultatL.taille = compteur; //taille de la suite (3, 4, 5 ou 6)
+                    return resultatL;
+                }
+            }else compteur = 1; // reinitialisation
+        }
+    }
+    return resultatL; //aucune suite trouvée
+}
+
+
+eliminerSuiteEnLigne(int ligne, int colonne, int taille) { //utiliser resultatL
 	for (int i=0; i<taille; i++) {                      
 		grille[ligne][colonne + i] = 0; //Parcourir les cases de la suite de gauche à droite
 	}	
 }
 
-eliminerSuiteEnColonne(int ligne, int colonne, int taille) {
+ResultatSuite detecterSuiteEnColonne(char grille[HAUTEUR][LARGEUR]){
+    int i, j;
+    ResultatSuite resultatC = {0, -1, -1, 0}; //initialisation du retour de la fonction avec des valeurs invalides (-1) pour ne pas créer de bug
+    //boucle for pour parcourir les colonnes donc la largeur
+    for(j=0; j<LARGEUR; j++){
+        int compteur = 1; //sert à mesurer la longueur d’une suite d’items identiques consécutifs. s'incrémente quand deux cases consécutives sont égales
+        //boucle for pour parcourir les lignes donc la hauteur
+        for(i=1; i<HAUTEUR; i++){
+            if(grille[i][j]==grille[i-1][j] && grille[i][j] != ' '){ //comparaison de deux cases consécutives
+                compteur++;
+
+                if(compteur>=3){//on a une suite valide si elle fait au moins 3 cases
+                    resultatC.trouve = 1;
+                    resultatC.ligne = i-compteur + 1; //on veut le numero de la première ligne où commence la suite
+                    resultatC.colonne = j;
+                    resultatC.taille = compteur; //taille de la suite (3, 4, 5 ou 6)
+                    return resultatC;
+                }
+            }else compteur = 1; //reinitialisation si les deux cases consécutives ne sont pas identiques
+        }
+    }
+    return resultatC; //aucune suite trouvée
+}
+
+eliminerSuiteEnColonne(int ligne, int colonne, int taille) { //utiliser resultatC
 	for (int i=0; i<taille; i++) {
 		grille[ligne + i][colonne] = 0; //Parcourir les cases de la suite de haut en bas et affecter 0 à chacune de ces cases
 	}	
