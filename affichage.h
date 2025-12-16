ccc9256f9b956c542eac77916ea3119061af9c8d
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-16 10:41:13 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -1,1 +1,2 @@
 void afficherNumeroNiveau(int niveau); //afficher le numero du niveau (1 2 ou 3)
+void afficherNombredeVies(int *nb_vies_restantes); //afficher le nombre de vies restantes du joueur à tout moment
