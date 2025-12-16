ab20dfba8e2959b33390b9df34f9fd404844d78b
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-16 10:25:43 +0100

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -3,6 +3,7 @@
 #include <conio.h>
 
 void afficherNumeroNiveau(int niveau){ //afficher le numero du niveau (1 2 ou 3)
+    gotoxy(0, 1); //position ligne 1
     printf("Niveau actuel : %d\n", niveau);
 } 
 
@@ -14,7 +15,7 @@ void afficherTempsrestant(int tempsrestant){ //afficher le temps restant pour ch
     while(tempsrestant>=0){
         int minutes = tempsrestant / 60;
         int secondes = tempsrestant %60;
-        system("cls");  //effacer l'ecran à chaque seconde
+        gotoxy(0, 2); //position ligne 2
         printf("Temps restant : %02d:%02d\n", minutes, secondes); //affichage du temps restant avec deux chiffres pour minutes et secondes
         Sleep(1000); //attendre 1 sec
         tempsrestant--;
@@ -23,8 +24,9 @@ void afficherTempsrestant(int tempsrestant){ //afficher le temps restant pour ch
 }
 
 void afficherRegles(){
+    gotoxy(0, 3); //position ligne 3
     printf("======= Regles du jeu =======\n");
-    printf("Le but du jeu est d'éliminer le plus d'items possible lors d'un niveau. Il y a 3 niveaux par partie, pour chaque niveau, l'utilisateur doit répondre à un contrat luiindiquant le nombre d'items à éliminer et le temps imparti.\n");
-    printf("Chaque niveau peut être sauvegardé avec un pseudo utilisateur, afin d'y retourner plus tard.\n");
+    printf("Le but du jeu est d'eliminer le plus d'items possible lors d'un niveau. Il y a 3 niveaux par partie, pour chaque niveau, l'utilisateur doit repondre à un contrat lui indiquant le nombre d'items a eliminer et le temps imparti.\n");
+    printf("Chaque niveau peut etre sauvegarde avec un pseudo utilisateur, afin d'y retourner plus tard.\n");
     printf("A vous de jouer !\n");
 }
