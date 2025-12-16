9385c3759ed16494050499362f154eb586b22302
Céline <celine.marco92@gmail.com>
2025-12-16 08:39:11 +0100

    Add function declarations for game mechanics

--- jeu.h
+++ jeu.h
@@ -1,8 +1,11 @@
+void viderGrille ();
+int verifierAbsencesFiduresInitiales ();
 void eliminerSuiteEnLigne(int ligne, int colonne, int taille);
 void eliminerSuiteEnColonne(int ligne, int colonne, int taille);
 void eliminerCarre (int ligne, int colonne);
 void eliminerCroix (int ligne, int colonne);
 void eliminerFigure (int type, int ligne, int colonne, int taille);
+void appliquerGravite ();
 void melangerItems ();
 int jeu (int temps_restant);
 
