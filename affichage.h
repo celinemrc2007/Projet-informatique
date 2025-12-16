b672ece8293ecc33b7cb0df7be059aa3e04ed57a
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-16 10:43:45 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -2,3 +2,4 @@ void afficherNumeroNiveau(int niveau); //afficher le numero du niveau (1 2 ou 3)
 void afficherNombredeVies(int *nb_vies_restantes); //afficher le nombre de vies restantes du joueur à tout moment
 void afficherTempsrestant(int tempsrestant); //afficher le temps restant pour chaque niveau en seconde
 void afficherRegles(); //afficher les règles du jeu lorsque souhaité par l'utilisateur
+void afficherGrille (char grille[HAUTEUR][LARGEUR]); //afficher la grille de jeu dans la hauteur et la largeur sont constantes
