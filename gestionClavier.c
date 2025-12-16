7d86937b470e3d0fad2780bfe87043bc538c2cf3
julieishak <julieishak20@gmail.com>
2025-12-16 23:33:03 +0100

    Update gestionClavier.c

--- gestionClavier.c
+++ gestionClavier.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+void selectionItem(int *ligne, int *colonne)
+{
+    printf("Ligne : ");
+    scanf("%d", ligne);
+    printf("Colonne : ");
+    scanf("%d", colonne);
+}
+
+void deplacementTouches(char touche, int *ligne, int *colonne)
+{
+    if (touche == 'z' && *ligne > 0) (*ligne)--;
+    if (touche == 's' && *ligne < 9) (*ligne)++;
+    if (touche == 'q' && *colonne > 0) (*colonne)--;
+    if (touche == 'd' && *colonne < 8) (*colonne)++;
+}
