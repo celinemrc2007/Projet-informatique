c7e0b1e1716a7e7418a3fdc9dff1af36dbabd36b
julieishak <julieishak20@gmail.com>
2025-12-17 17:15:15 +0100

    Update gestionClavier.h

--- gestionClavier.h
+++ gestionClavier.h
@@ -1,2 +1,3 @@
 void deplacementTouches(char touche, int *ligne, int *colonne); //attribuer une touche par mouvement possible
 void selectionItem(int *ligne, int *colonne); //sélectionner un item et indiquer avec quel item il échange de place
+void malus(int *temps_fin, int ligne, int colonne)
