231446c37fd14cde38b3ae0998869b44a598b8e2
Céline <celine.marco92@gmail.com>
2025-12-17 18:43:03 +0100

    Refactor function signatures in gestionClavier.h

--- gestionClavier.h
+++ gestionClavier.h
@@ -1,3 +1,4 @@
-void deplacementTouches(char touche, int *ligne, int *colonne); //attribuer une touche par mouvement possible
-void selectionItem(int *ligne, int *colonne); //sélectionner un item et indiquer avec quel item il échange de place
-void malus(int *temps_fin, int ligne, int colonne);
+int ligneCurseur ();
+int colonneCurseur ();
+void deplacementTouches(char touche);
+bool selectionItem (char touche, int *l1, int *c1, int *l2, int *c2);
