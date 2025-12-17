382c4ae74d8dfb90d6466a4df6c27562a506c123
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-17 09:57:06 +0100

    Update gestionNiveau.h

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -1,1 +1,3 @@
-void lancerNiveau(int xcurseur, int ycurseur, int temps_restant);
+void lancerNiveau(int xcurseur, int ycurseur, int temps_restant); //appeler la fonction jeu et toutes les fonctions du module affichage
+
+
