c7be0e1605364a56db55b0fb767d29605376702a
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-17 09:58:08 +0100

    Update gestionNiveau.h

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -1,3 +1,3 @@
 void lancerNiveau(int xcurseur, int ycurseur, int temps_restant); //appeler la fonction jeu et toutes les fonctions du module affichage
-
+void gerePerteVies(int *nb_vies_restantes); //retirer une vie si le joueur a écoulé l'entièreté du temps ou des coups dont il disposait
 
