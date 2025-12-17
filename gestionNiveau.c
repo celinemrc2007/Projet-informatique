1e0f4b2bf80c8cab4241b116cf41acbe98abd0d1
julieishak <julieishak20@gmail.com>
2025-12-17 09:26:45 +0100

    Update gestionNiveau.c

--- gestionNiveau.c
+++ gestionNiveau.c
@@ -41,24 +41,5 @@ void gerePerteVies(int *nb_vies_restantes){
         (*nb_vies_restantes)--; //utilisateur perd une vie s'il lui en reste
     }
 }
-void genererItems(char grille[10][9])
-{
-    char items[5] = {'X','O','&','@','%'};
-    int i, j;
 
-    srand(time(NULL));
-
-    for (i = 0; i < 10; i++)
-        for (j = 0; j < 9; j++)
-            grille[i][j] = items[rand() % 7];
-}
-
-void gererContrat(char item, int *x, int *o, int *et, int *at, int *pourcent)
-{
-    if (item == 'X' && *x > 0) (*x)--;
-    if (item == 'O' && *o > 0) (*o)--;
-    if (item == '&' && *et > 0) (*et)--;
-    if (item == '@' && *at > 0) (*at)--;
-    if (item == '%' && *pourcent > 0) (*pourcent)--;
-}	
 
