e75445c4a9b3177d28358d88ec342c91a4028a12
Céline <celine.marco92@gmail.com>
2025-12-17 10:22:17 +0100

    Comment out game management functions in header
    Comment out unused functions related to game management.

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -1,8 +1,8 @@
 void lancerNiveau(int xcurseur, int ycurseur, int temps_restant); //appeler la fonction jeu et toutes les fonctions du module affichage
-void Bonus ();
-void Malus ();
-void gererVictoireNiveau ();
-void gererEchecNiveau (int vies_restantes);
-void gererVictoireNiveau ();
-void gererEchecNiveau (int vies);
+//void Bonus ();
+//void Malus ();
+//void gererVictoireNiveau ();
+//void gererEchecNiveau (int vies_restantes);
+//void gererVictoireNiveau ();
+//void gererEchecNiveau (int vies);
 int gerePerteVies(int *nb_vies_restantes); //retirer une vie si le joueur a écoulé l'entièreté du temps ou des coups dont il disposait
