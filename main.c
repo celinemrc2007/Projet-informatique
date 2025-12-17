50dd4ed67a344207b37242879833e776fc9d3aa0
Céline <celine.marco92@gmail.com>
2025-12-17 09:48:09 +0100

    Add menu function to display game options

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+//Afficher le menu
+int menu() {
+    int choix;
+    text_color(FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN);
+    printf("\n1. Jouer");
+    printf("\n2. Regles du jeu");
+    printf("\n3. Quitter\n");
+    scanf("%d",&choix);
+    return choix;
+}
 
