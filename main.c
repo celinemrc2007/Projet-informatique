9b8a0f43579896016fe8fa36975c84a79bc2554a
julieishak <julieishak20@gmail.com>
2025-12-17 10:02:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,8 @@ int menu() {
     int choix;
     text_color(FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN);
     printf("===== MENU PRINCIPAL =====\n");
-    printf("1. Lire les regles\n");
-    printf("2. Nouvelle partie\n");
+    printf("1. Lire les regles du jeu\n");
+    printf("2. Commencer une nouvelle partie\n");
     printf("3. Reprendre une partie\n");
     printf("4. Quitter\n");
     printf("Votre choix : ");
