a79bbecfaab2229a04394ec9efac81dbc897e9dd
Céline <celine.marco92@gmail.com>
2025-12-17 09:50:41 +0100

    Implement main function and game menu
    Add main function with menu handling and game loop.

--- main.c
+++ main.c
@@ -8,4 +8,27 @@ int menu() {
     scanf("%d",&choix);
     return choix;
 }
+int main() {
+    int choix;
+    // initialisation du générateur pseudo-aléatoire
+    srand(time(NULL));
+    //boucle de retour au menu à la fin d'une partie
+    do{
+        //gestion du menu
+        choix=menu();
+        switch(choix){
+            case 1 : /*lancement d'une partie*/
+                    //hide_cursor();
+                    clrscr();
+                    jouer();
+                    //show_cursor();
+                    break;
+            case 2 :
+                    afficherRegles();
+                    break;
+        }
+    }while(choix!=3);
+
+    return 0;
+}
 
