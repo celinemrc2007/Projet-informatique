c493c4775d381f248246c5b247b34c68f9a516e6
Céline <celine.marco92@gmail.com>
2025-12-18 23:14:01 +0100

    Update gestionNiveau.c

--- gestionNiveau.c
+++ gestionNiveau.c
@@ -1,4 +1,5 @@
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,29 +9,21 @@
 #include "sauvegarde.h"
 #include "gestionClavier.h"
 #include "sauvegarde.h"
-#include "gestionNiveaux.h"
+#include "gestionNiveau.h"
+#include "jeu.h"
+
 
 #define HAUTEUR 10
 #define LARGEUR 9
 #define DUREE 120000
 int niveau=1;
 
-int get_niveau() { return niveau}
+int *copie_contrat;
 
-void lancerNiveau(int xcurseur, int ycurseur, int temps_restant, int compteurs_contrat[7]) {
-	xcurseur=LARGEUR/2;
-	ycurseur=HAUTEUR/2;
-	jeu(temps_restant);
-	viderGrille();
-	genererItems();
-	assurerGrilleJouable();
-	afficherGrille();
-	afficherNumeroDuNiveau(niveau);
-	afficherNombreDeVies(vies);
-	afficherTempsRestants(temps_fin, temps_restant);
-	afficherCoupsRestants(coups_restants);
-	afficherRegles();
-	afficherItemsRestants(type_items);
+int get_niveau() { return niveau; }
+
+void lancerNiveau(int compteurs_contrat[7]) {
+	copie_contrat = compteurs_contrat;
 
 	switch(niveau) {
 		case 1: // Definir 1e contrat du niveau 1
@@ -46,28 +39,38 @@ void lancerNiveau(int xcurseur, int ycurseur, int temps_restant, int compteurs_c
 			for (int i=4 ; i<=5 ; i++) compteurs_contrat[i] = 12;
 			break;
 	}
+	afficherNumeroNiveau(niveau);
+	jeu();
 }
 
-void gerePerteVies(int *vies_restantes){
-    if (*nb_vies_restantes > 0) {
-        (*nb_vies_restantes)--; //utilisateur perd une vie s'il lui en reste
+void gererPerteVies(int *vies_restantes){
+    if (*vies_restantes > 0) {
+        (*vies_restantes)--; //utilisateur perd une vie s'il lui en reste
     }
 }
-void gereVictoireNiveau(){
+
+void gererVictoireNiveau(){
 	printf("###############################################\n");
 	printf("#                                             #\n");
 	printf("     ! Bravo vous avez gagne le niveau !       \n");
 	printf("#                                             #\n");
 	printf("###############################################\n");	
 	Sleep(5000); //affichage du message pendant 5 sec
+	gererVictoirePartie (niveau);
 }
-void gererEchecNiveau(){
+void gererEchecNiveau(int *vies_restantes){
 	printf("###############################################\n");
 	printf("#                                             #\n");
 	printf("      Dommage vous avez perdu le niveau :(     \n");
 	printf("#                                             #\n");
 	printf("###############################################\n");
 	Sleep(5000);
+	gererPerteVies(vies_restantes);
+	if(*vies_restantes == 0) {
+		gererEchecPartie (vies_restantes);
+	} else {
+		//reprendrePartie ();
+	}
 }
 void gererVictoirePartie(int niveau){
     if (niveau == 3){
@@ -81,29 +84,27 @@ void gererVictoirePartie(int niveau){
         printf("#############################################\n");
         printf("\nRetour au menu principal...\n");
         Sleep(10000); // pause 10 secondes
-        menu(); // retour au menu
     }
 }
-void gererEchecPartie(){
-    int vies = gererPerteVie();
-    if (vies > 0){
+void gererEchecPartie(int *vies_restantes){
+    gererPerteVies(vies_restantes);
+    if (*vies_restantes > 0){
         char choix;
-        printf("Echec de la partie. Il vous reste %d vie(s).\n", vies);
+        printf("Echec de la partie. Il vous reste %d vie(s).\n", *vies_restantes);
         printf("Relancer le niveau ? (O/N)\n");
-        choix = getch();
+        scanf("%c", &choix);
         if (choix == 'O' || choix == 'o'){
-            lancerNiveau();
+            lancerNiveau(copie_contrat);
         }
         else{
-            menu();
+          // A faire pb d'organisation menu(); // retour au menu  menu();
         }
-    }
-    else{
+
+    } else{
         printf("GAME OVER !\n");
         Sleep(3000);
-        menu();
-void gerePerteVies(int *vies_restantes){
-    if (*nb_vies_restantes > 0) {
-        (*nb_vies_restantes)--; //utilisateur perd une vie s'il lui en reste
+       // A faire pb d'organisation menu(); // retour au menu menu();
     }
 }
+
+
