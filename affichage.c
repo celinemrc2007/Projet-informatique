3ed0d595d494da995c8be3c2e7ada64a72e87eb6
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-18 18:20:21 +0100

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <windows.h>
-#include <conio.h>
-#define HAUTEUR 10
-#define LARGEUR 9
+#include "affichage.h"
 ##include <stdio.h>
 #include <windows.h>
 #include <conio.h>
