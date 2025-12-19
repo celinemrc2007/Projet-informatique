747907ac215ab428a37168e8ad997979173b8f4a
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-19 09:46:44 +0100

    Update gestionNiveau.h

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -1,6 +1,5 @@
+void gererBonusNiveau3(ResultatFigure fig);
 void lancerNiveau(int compteurs_contrat[7]); //appeler la fonction jeu et toutes les fonctions du module affichage
-//void bonus ();
-//void malus ();
 void gererVictoireNiveau ();
 void gererEchecNiveau (int *vies_restantes);
 void gererPerteVies(int *vies_restantes); //retirer une vie si le joueur a écoulé l'entièreté du temps ou des coups dont il disposait
