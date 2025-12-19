74892be053dc3cce104c41301de2a48731119268
Céline <celine.marco92@gmail.com>
2025-12-20 00:59:15 +0100

    Update gestionNiveau.h

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -1,13 +1,12 @@
-void gererBonusNiveau3(ResultatFigure fig);
-void lancerNiveau(int compteurs_contrat[7]); //appeler la fonction jeu et toutes les fonctions du module affichage
-void gererVictoireNiveau ();
-void gererEchecNiveau (int *vies_restantes);
-void gererPerteVies(int *vies_restantes); //retirer une vie si le joueur a écoulé l'entièreté du temps ou des coups dont il disposait
-void gererVictoirePartie(int niveau);
-void gererEchecPartie(int *vies_restantes);
-void quitterProgramme();
-
-
+// void lancerNiveau(int compteurs_contrat[7]); //appeler la fonction jeu et toutes les fonctions du module affichage
+// //void bonus ();
+// //void malus ();
+// void gererVictoireNiveau ();
+// void gererEchecNiveau (int *vies_restantes);
+// void gererPerteVies(int *vies_restantes); //retirer une vie si le joueur a écoulé l'entièreté du temps ou des coups dont il disposait
+// void gererVictoirePartie(int niveau);
+// void gererEchecPartie(int *vies_restantes);
+// void quitterProgramme();
 
 //*********************NOUVELLE VERSION****************************//
 #ifndef GESTION_NIVEAU_H
@@ -25,13 +24,13 @@ int get_niveau(void);
 
 void lancerNiveau(int compteurs_contrat[7]);
 
-void gererPerteVies(int *vies_restantes);
+void gererPerteVies();
 
 void gererVictoireNiveau(void);
-void gererEchecNiveau(int *vies_restantes);
+void gererEchecNiveau();
 
 void gererVictoirePartie(int niveau_final);
-void gererEchecPartie(int *vies_restantes);
+void gererEchecPartie();
 
 void gererBonusNiveau3(ResultatFigure fig);
 
