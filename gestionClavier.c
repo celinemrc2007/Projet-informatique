32f1ccf89c8b50b579eb1fd66edf5b6d33215c25
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-19 14:54:45 +0100

    Update gestionClavier.c

--- gestionClavier.c
+++ gestionClavier.c
@@ -72,3 +72,109 @@ void deplacementTouches(char touche){
             break;
     }
 }
+
+
+
+
+
+//*********************NOUVELLE VERSION****************************//
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <stdbool.h>   // pour bool
+#include "gestionClavier.h"
+
+/* Dimensions de la grille */
+#define HAUTEUR 10
+#define LARGEUR 9
+
+/* ================= VARIABLES GLOBALES ================= */
+
+/* Curseur utilisé par affichage.c */
+int curseurL = HAUTEUR / 2;
+int curseurC = LARGEUR / 2;
+
+/* ================= GETTERS POUR AFFICHAGE ================= */
+
+int ligneCurseur(void) {
+    return curseurL;
+}
+
+int colonneCurseur(void) {
+    return curseurC;
+}
+
+/* ================= GESTION SELECTION ================= */
+
+/*
+ * Retourne true si une tentative d'échange est demandée
+ * l1,c1 : position initiale
+ * l2,c2 : position cible
+ */
+bool selectionItem(char touche, int *l1, int *c1, int *l2, int *c2)
+{
+    *l1 = curseurL;
+    *c1 = curseurC;
+    *l2 = curseurL;
+    *c2 = curseurC;
+
+    switch (touche) {
+        case 'q': // gauche
+            if (curseurC > 0) {
+                (*c2)--;
+                return true;
+            }
+            break;
+
+        case 's': // droite
+            if (curseurC < LARGEUR - 1) {
+                (*c2)++;
+                return true;
+            }
+            break;
+
+        case 'z': // haut
+            if (curseurL > 0) {
+                (*l2)--;
+                return true;
+            }
+            break;
+
+        case 'w': // bas
+            if (curseurL < HAUTEUR - 1) {
+                (*l2)++;
+                return true;
+            }
+            break;
+    }
+
+    return false;
+}
+
+/* ================= DEPLACEMENT DU CURSEUR ================= */
+
+void deplacementTouches(char touche)
+{
+    switch (touche) {
+        case 'q': // gauche
+            if (curseurC > 0)
+                curseurC--;
+            break;
+
+        case 's': // droite
+            if (curseurC < LARGEUR - 1)
+                curseurC++;
+            break;
+
+        case 'z': // haut
+            if (curseurL > 0)
+                curseurL--;
+            break;
+
+        case 'w': // bas
+            if (curseurL < HAUTEUR - 1)
+                curseurL++;
+            break;
+    }
+}
