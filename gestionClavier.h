8e6eceba4e9854c828ed7845da484c223d2df828
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-19 14:55:19 +0100

    Update gestionClavier.h

--- gestionClavier.h
+++ gestionClavier.h
@@ -3,3 +3,27 @@ int ligneCurseur ();
 int colonneCurseur ();
 void deplacementTouches(char touche);
 bool selectionItem (char touche, int *l1, int *c1, int *l2, int *c2);
+
+
+
+//*********************NOUVELLE VERSION****************************//
+#ifndef GESTION_CLAVIER_H
+#define GESTION_CLAVIER_H
+
+#include <stdbool.h>
+
+/* Curseur partagé avec affichage.c */
+extern int curseurL;
+extern int curseurC;
+
+/* Accès curseur pour affichage */
+int ligneCurseur(void);
+int colonneCurseur(void);
+
+/* Gestion clavier */
+void deplacementTouches(char touche);
+
+/* Sélection / tentative de permutation */
+bool selectionItem(char touche, int *l1, int *c1, int *l2, int *c2);
+
+#endif
