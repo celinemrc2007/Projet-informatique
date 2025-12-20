c36dc5ca0efd4b37c3c234216aeaaf0d9c89e2be
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-20 16:41:15 +0100

    Update gestionNiveau.h

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -26,16 +26,25 @@ extern int vies_restantes;
 
 int get_niveau(void);
 
+/* Initialise le contrar en fonction du niveau joué, lance le jeu */
 void lancerNiveau(int compteurs_contrat[7]);
 
+/* Gère la gestion de la perte de vie */
 void gererPerteVies();
 
-void gererVictoireNiveau(void);
+/* Gère lorsque le joueur gagne un niveau */
+void gererVictoireNiveau();
+
+/* Gère lorsque le joueur perd un niveau */
 void gererEchecNiveau();
 
+/* Gère lorsque le joueur gagne la partie */
 void gererVictoirePartie();
+
+/* Gère lorsque le joueur perd la partie */
 void gererEchecPartie();
 
+/* Assure la mise en place du bonus au niveau 3 */
 void gererBonusNiveau3(ResultatFigure fig);
 
 #endif
