b25adbdb2bde96089d533b43937ec3c512178b04
Céline <celine.marco92@gmail.com>
2025-12-20 19:08:47 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,25 +1,18 @@
 
-void viderGrille ();
-void assurerGrilleJouable ();
-int verifierAbsencesFiduresInitiales ();
-void appliquerGravite ();
-void melangerItems ();
-int jeu ();
-void genererItems();
-int gererContrat();
-
-
-
-
-
+// void viderGrille ();
+// void assurerGrilleJouable ();
+// int verifierAbsencesFiduresInitiales ();
+// void appliquerGravite ();
+// void melangerItems ();
+// int jeu ();
+// void genererItems();
+// int gererContrat();
 
 
 //*******************************NOUVELLE VERSION**********************//
 #ifndef JEU_H
 #define JEU_H
 
-#define HAUTEUR 12
-#define LARGEUR 11
 #define PAS_DE_FIGURE 0
 #define SUITE_LIGNE 1
 #define SUITE_COLONNE 2
@@ -28,14 +21,6 @@ int gererContrat();
 #define TAILLE_CARRE 4
 #define TAILLE_CROIX 5
 
-/* ========================== VARIABLES =======================*/
-
-extern int grille[HAUTEUR][LARGEUR];
-extern int compteurs_contrat[7];
-extern int coups_restants;
-extern int malusRouge[HAUTEUR][LARGEUR];
-
-/* ========================== STRUCTURE =========================*/
 typedef struct {
     int type;
     int ligne;
@@ -43,90 +28,33 @@ typedef struct {
     int taille;
 } ResultatFigure;
 
-/* ========================== OUTILS ====================*/
-
-/* Vide le contenu de la grille de jeu */
+// prototypes
 void viderGrille(void);
-
-/* Génèrer des items aléatoirement */
 void genererItems(void);
-
-/* Vide le contenu d'une case de jeu */
 void viderCase(int i, int j);
-
-/* ====================== CONTRAT ================*/
-
-/* Gère l'avancement du contrat */
 int gererContrat(void);
-
-/* ============== DETECTION / ELIMINATION ============== */
-
-/* Detecte une suite en ligne  d'au moins 3 items */
 ResultatFigure detecterSuiteEnLigne(void);
-
-/* Detecte une suite en colone d'au moin 3 items */
 ResultatFigure detecterSuiteEnColonne(void);
-
-/* Detecte un carré de 4 items de côté */
 ResultatFigure detecterCarre(void);
-
-/* Detecte une croix de 5 items par branche */
 ResultatFigure detecterCroix(void);
-
-/* Regroupe toutes les detections */
 ResultatFigure detecterFigure(void);
-
-/* Elimine suite en ligne */
-void eliminerSuiteEnLigne(ResultatFigure resultatL);
-
-/* Elimine suite en colonne */
-void eliminerSuiteEnColonne(ResultatFigure resultatC);
-
-/* Elimine carré */
-void eliminerCarre(ResultatFigure resultatCr);
-
-/* Elimine croix*/
-void eliminerCroix(ResultatFigure resultatCx);
-
-/* Elimine la bonne figure selon les combinaisons sur la grille de jeu */
-void eliminerFigure(ResultatFigure resultatglobal);
-
-/* ================= GRAVITE ================*/
-
-/* Assure que la ligne est vide */
-bool ligneNonPleine(int ligne);
-
-/* Fait tomber les items du haut */
+void eliminerSuiteEnLigne(ResultatFigure f);
+void eliminerSuiteEnColonne(ResultatFigure f);
+void eliminerCarre(ResultatFigure f);
+void eliminerCroix(ResultatFigure f);
+void eliminerFigure(ResultatFigure f);
 void appliquerGravite(void);
-
-/* Assure qu'il n'y a pas de figure initiale */
-void assurerAbsencesFiguresInitiales ();
-
-/* ================ GRILLE JOUABLE =============*/
-
-/* Verifie si la grille est jouable */
-bool verifierGrilleJouable();
-void assurerGrilleJouable();
-
-/* ================== PERMUTATION =============*/
-
-/* Permute l'emplacement des items */
-void permuterItems(int l1, int c1, int l2, int c2);
-
-/* Assure que la permutation soit valide */
-int permuterSiValide(int l1, int c1, int l2, int c2);
-
-/* Melange les items de manière aléatoire */
 void melangerItems(void);
-
-/* =================== BOUCLE DE JEU =============== */
-
-/* Organise le déroulement du jeu pour chaque niveau */
+int permuterSiValide(int l1, int c1, int l2, int c2);
+void permuterItems(int l1, int c1, int l2, int c2);
 int jeu(void);
 
 
-
-
+extern int grille[HAUTEUR][LARGEUR];
+extern int compteurs_contrat[7];
+extern int coups_restants;
+extern int temps_restant;
+extern int malusRouge[HAUTEUR][LARGEUR];
 
 #endif
 
