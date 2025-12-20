2b3236069409d1acae95e977ff60b618751b99a2
Céline <celine.marco92@gmail.com>
2025-12-20 21:23:20 +0100

    Update gestionClavier.c

--- gestionClavier.c
+++ gestionClavier.c
@@ -1,89 +1,10 @@
-// #include <stdio.h>
-// #include <conio.h>
-// #include <windows.h>
-// #include <stdlib.h>
-
-// #define HAUTEUR 10
-// #define LARGEUR 9
-
-// //variables globales 
-// int curseurL = HAUTEUR/2;
-// int curseurC = LARGEUR/2;
-
-// int ligneCurseur() { return curseurL; }
-// int colonneCurseur() { return curseurC; }
-
-// Retourne true si le joueur essaie d'échanger 2 items
-// bool selectionItem (char touche, int *l1, int *c1, int *l2, int *c2) {
-//     *l1 = curseurL;
-//     *c1 = curseurC;
-//     *l2 = curseurL;
-//     *c2 = curseurC;
-//     switch(touche){
-//         case 'Q': // gauche
-//             if (curseurC > 0) {
-//                 *c2--;
-//                 return true;
-//             }
-//             break;
-//         case 'S': // droite
-//             if (curseurC < LARGEUR - 1) {
-//                 *c2++;
-//                 return true;
-//             }
-//             break;
-//         case 'Z': // haut
-//             if (curseurL > 0) {
-//                 *l2--;
-//                 return true;
-//             }
-//             break;
-//         case 'W': // bas
-//             if (curseurL < HAUTEUR - 1) {               
-//                 *l2++;
-//                 return true;
-//             }
-//             break;
-//     }
-//     return false;
-// }
-
-
-// void deplacementTouches(char touche){
-//     switch(touche){
-//         case 'q': // gauche
-//             if (curseurC > 0)
-//                 curseurC--;
-//             break;
-
-//         case 's': // droite
-//             if (curseurC < LARGEUR - 1)
-//                 curseurC++;
-//             break;
-
-//         case 'z': // haut
-//             if (curseurL > 0)
-//                 curseurL--;
-//             break;
-
-//         case 'w': // bas
-//             if (curseurL < HAUTEUR - 1)
-//                 curseurL++;
-//             break;
-//     }
-// }
-
-//*********************NOUVELLE VERSION****************************//
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
 #include <stdlib.h>
 #include <stdbool.h>   // pour bool
 #include "gestionClavier.h"
-
-/* Dimensions de la grille */
-#define HAUTEUR 10
-#define LARGEUR 9
+#include "affichage.h"
 
 /* ================= VARIABLES GLOBALES ================= */
 
@@ -91,7 +12,7 @@
 int curseurL = HAUTEUR / 2;
 int curseurC = LARGEUR / 2;
 
-/* ================= GETTERS POUR AFFICHAGE ================= */
+/* ================= AFFICHAGE DU CURSEUR ================= */
 
 int ligneCurseur(void) {
     return curseurL;
@@ -101,7 +22,6 @@ int colonneCurseur(void) {
     return curseurC;
 }
 
-/* ================= GESTION SELECTION ================= */
 
 // Retourne true si le joueur essaie d'échanger 2 items
 bool selectionItem (char touche, int *l1, int *c1, int *l2, int *c2) {
