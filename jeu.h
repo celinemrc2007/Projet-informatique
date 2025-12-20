07358acf695d536934d503bbb8b463d732a62bea
LinaBaghli <lina.baghli@edu.ece.fr>
2025-12-20 18:24:27 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -18,8 +18,8 @@ int gererContrat();
 #ifndef JEU_H
 #define JEU_H
 
-#define HAUTEUR 10
-#define LARGEUR 9
+#define HAUTEUR 12
+#define LARGEUR 11
 #define PAS_DE_FIGURE 0
 #define SUITE_LIGNE 1
 #define SUITE_COLONNE 2
