975f9f6d711c69b13576967075a5ac35752bd9ff
Céline <celine.marco92@gmail.com>
2025-12-20 21:34:26 +0100

    Update gestionNiveau.h

--- gestionNiveau.h
+++ gestionNiveau.h
@@ -1,15 +1,3 @@
-
-// void lancerNiveau(int compteurs_contrat[7]); //appeler la fonction jeu et toutes les fonctions du module affichage
-// //void bonus ();
-// //void malus ();
-// void gererVictoireNiveau ();
-// void gererEchecNiveau (int *vies_restantes);
-// void gererPerteVies(int *vies_restantes); //retirer une vie si le joueur a écoulé l'entièreté du temps ou des coups dont il disposait
-// void gererVictoirePartie(int niveau);
-// void gererEchecPartie(int *vies_restantes);
-// void quitterProgramme();
-
-//*********************NOUVELLE VERSION****************************//
 #ifndef GESTION_NIVEAU_H
 #define GESTION_NIVEAU_H
 
