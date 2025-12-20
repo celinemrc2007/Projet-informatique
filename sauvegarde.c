6bd1af5778998761aba561d93d683259931e1ec5
Céline <celine.marco92@gmail.com>
2025-12-20 11:53:52 +0100

    Update sauvegarde.c

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,98 +1,98 @@
-#include <stdio.h>
-#include <string.h>
-#include "sauvegarde.h"
+// #include <stdio.h>
+// #include <string.h>
+// #include "sauvegarde.h"
 
-/* Format du fichier texte (1 sauvegarde par ligne)
-pseudo;niveau;vies
-ex: Alice;3;2
-*/
+// /* Format du fichier texte (1 sauvegarde par ligne)
+// pseudo;niveau;vies
+// ex: Alice;3;2
+// */
 
-void sauvegarderPartie(const char *pseudo, int niveau, int vies) {
-FILE *f = fopen(FICHIER_SAUVEGARDE, "r");
-FILE *temp = fopen("temp.txt", "w");
-
-
-char p[MAX_PSEUDO];
-int n, v;
-int trouve = 0;
+// void sauvegarderPartie(const char *pseudo, int niveau, int vies) {
+// FILE *f = fopen(FICHIER_SAUVEGARDE, "r");
+// FILE *temp = fopen("temp.txt", "w");
 
 
-if (f != NULL) {
-while (fscanf(f, "%49[^;];%d;%d\n", p, &n, &v) == 3) {
-if (strcmp(p, pseudo) == 0) { //si le pseudo existe -> met à jour la ligne 
-fprintf(temp, "%s;%d;%d\n", pseudo, niveau, vies); 
-trouve = 1;
-} else {
-fprintf(temp, "%s;%d;%d\n", p, n, v); //sinon -> ajoute une nouvelle ligne
-}
-}
-fclose(f);
-}
+// char p[MAX_PSEUDO];
+// int n, v;
+// int trouve = 0;
 
 
-if (!trouve) {
-fprintf(temp, "%s;%d;%d\n", pseudo, niveau, vies);
-}
+// if (f != NULL) {
+// while (fscanf(f, "%49[^;];%d;%d\n", p, &n, &v) == 3) {
+// if (strcmp(p, pseudo) == 0) { //si le pseudo existe -> met à jour la ligne 
+// fprintf(temp, "%s;%d;%d\n", pseudo, niveau, vies); 
+// trouve = 1;
+// } else {
+// fprintf(temp, "%s;%d;%d\n", p, n, v); //sinon -> ajoute une nouvelle ligne
+// }
+// }
+// fclose(f);
+// }
 
 
-fclose(temp);
+// if (!trouve) {
+// fprintf(temp, "%s;%d;%d\n", pseudo, niveau, vies);
+// }
 
 
-remove(FICHIER_SAUVEGARDE);
-rename("temp.txt", FICHIER_SAUVEGARDE); //Utilise un fichier temporaire pour éviter de perdre les données
-}
+// fclose(temp);
 
 
-
-int chargerPartie(const char *pseudo, int *niveau, int *vies) {
-FILE *f = fopen(FICHIER_SAUVEGARDE, "r");
-char p[MAX_PSEUDO];
-int n, v;
-
-
-if (f == NULL)
-return 0; //pseudo inexistant
+// remove(FICHIER_SAUVEGARDE);
+// rename("temp.txt", FICHIER_SAUVEGARDE); //Utilise un fichier temporaire pour éviter de perdre les données
+// }
 
 
 
-while (fscanf(f, "%49[^;];%d;%d\n", p, &n, &v) == 3) { //recherche le pseudo dans le fichier
-if (strcmp(p, pseudo) == 0) {
-*niveau = n; //remplissage
-*vies = v; //remplissage
-fclose(f);
-return 1; //sauvegarde trouvée
-}
-}
+// int chargerPartie(const char *pseudo, int *niveau, int *vies) {
+// FILE *f = fopen(FICHIER_SAUVEGARDE, "r");
+// char p[MAX_PSEUDO];
+// int n, v;
 
 
-fclose(f);
-return 0;
-}
+// if (f == NULL)
+// return 0; //pseudo inexistant
 
 
 
-
-int pseudoExiste(const char *pseudo) { //verifie si le pseudo est déjà eregistré
-FILE *f = fopen(FICHIER_SAUVEGARDE, "r");
-char p[MAX_PSEUDO];
-int n, v;
-
-
-if (f == NULL)
-return 0;
-
-
-while (fscanf(f, "%49[^;];%d;%d\n", p, &n, &v) == 3) {
-if (strcmp(p, pseudo) == 0) {
-fclose(f);
-return 1;
-}
-}
+// while (fscanf(f, "%49[^;];%d;%d\n", p, &n, &v) == 3) { //recherche le pseudo dans le fichier
+// if (strcmp(p, pseudo) == 0) {
+// *niveau = n; //remplissage
+// *vies = v; //remplissage
+// fclose(f);
+// return 1; //sauvegarde trouvée
+// }
+// }
 
 
-fclose(f);
-return 0;
-}
+// fclose(f);
+// return 0;
+// }
+
+
+
+
+// int pseudoExiste(const char *pseudo) { //verifie si le pseudo est déjà eregistré
+// FILE *f = fopen(FICHIER_SAUVEGARDE, "r");
+// char p[MAX_PSEUDO];
+// int n, v;
+
+
+// if (f == NULL)
+// return 0;
+
+
+// while (fscanf(f, "%49[^;];%d;%d\n", p, &n, &v) == 3) {
+// if (strcmp(p, pseudo) == 0) {
+// fclose(f);
+// return 1;
+// }
+// }
+
+
+// fclose(f);
+// return 0;
+// }
 
 
 
